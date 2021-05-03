11424a0587f689ec18c07573bcf852566888e5da
Daniel Rivero <danielrivgw@gmail.com>
2021-05-03 18:22:40 -0400

    Task #3 - Working Project

---
+++ Cineplus/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cineplus.Models {
+	public class ApplicationUser : IdentityUser { }
+}
