5ad4fd4c2476ce23dc1269ae1157dfa830da6680
Jean Pierre Gomez Matos <jeangomezmatos@gmail.com>
2021-06-12 14:09:43 -0400

    CancelationMacro

--- Cineplus/ClientApp/src/app/client-profile/purchase-history/purchase-history.component.ts
+++ Cineplus/ClientApp/src/app/client-profile/purchase-history/purchase-history.component.ts
@@ -4,6 +4,7 @@ import {HttpClient} from "@angular/common/http";
 import {DataSourceConf} from "../../../models/dataSourceConf";
 import {MatDialog} from "@angular/material/dialog";
 import {Ticket} from "../../../models/ticket";
+import {TitleCasePipe} from "@angular/common";
 
 @Component({
   selector: 'app-purchase-history',
@@ -12,10 +13,9 @@ import {Ticket} from "../../../models/ticket";
 })
 export class PurchaseHistoryComponent implements OnInit {
 
-  orderDataSource: CineplusDataSource<Ticket[][]>;
+  orderDataSource: CineplusDataSource<Ticket[]>;
   propColumns: string[] = ['movie', 'teather', 'seat']
   allColumns: string[] = this.propColumns.concat('price')
-  panelOpenState: boolean;
 
   constructor(private http: HttpClient,
               @Inject('BASE_URL') private baseUrl: string,
@@ -24,7 +24,7 @@ export class PurchaseHistoryComponent implements OnInit {
 
     let dsConf = new DataSourceConf();
     dsConf.endPoint = baseUrl + 'api/ticket/order'
-    this.orderDataSource = new CineplusDataSource<Ticket[][]>(http, dsConf);
+    this.orderDataSource = new CineplusDataSource<Ticket[]>(http, dsConf);
     this.orderDataSource.refresh()
   }
 
@@ -35,6 +35,31 @@ export class PurchaseHistoryComponent implements OnInit {
     console.log($event)
     this.orderDataSource.currentPagination.pageSize = $event.pageSize;
     this.orderDataSource.setPage($event.pageIndex + 1);
+  }
+
+  calculatePrice(i: number)
+  {
+    let sum = 0;
+    let prop = (this.orderDataSource.result[i][0].price == 0) ? 'pointsPrice' : 'price';
+
+    for(let j = 0; j < this.orderDataSource.result[i].length; j++)
+      sum += this.orderDataSource.result[i][j][prop];
+
+    return (prop == 'price' ? "Price: $" : "Points: ") + sum;
+  }
+
+  canCancel(i: number)
+  {
+    let actualDate = new Date();
+    let startDate = this.orderDataSource.result[i][0].reproduction.startTime;
+
+    if(startDate < actualDate)
+      return -1;
+
+    if(startDate.valueOf() - actualDate.valueOf() < 7200000)
+      return 0;
+
+    return 1;
   }
 
   log(element)
--- Cineplus/ClientApp/src/models/ticket.ts
+++ Cineplus/ClientApp/src/models/ticket.ts
@@ -1,9 +1,12 @@
 ﻿import {Seat} from "./seat";
 import {Reproduction} from "./reproduction";
+import {User} from "./user";
 
 export class Ticket {
   seat: Seat;
+  pointsPrice: number;
   price: number;
+  user: User;
   reproductionId: number;
   reproduction: Reproduction;
 }
